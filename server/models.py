5f7f90718405cb1a4985ec675e3f987517d83c63
Baran <bar.yildiran@gmail.com>
2025-05-14 14:33:09 -0600

    a3

--- server/models.py
+++ server/models.py
@@ -23,7 +23,6 @@ class Well(db.Mode, SerializerMixin):
     # Foreign Keys
     
     # production_curve_id
-    # gas_concentration_id
     # assumptions_id
     # project_id
     # user_id --- for the future
@@ -60,6 +59,91 @@ class Assumptions(db.Model, SerializerMixin):
     # inlet_gas_fee ($/mcf)
     inlet_gas_fee = db.Column(db.Float)
     # nat gas liquids ($/gal)
+    ngl_processing_feee = db.Column(db.Float)
+
+    # additional revenue sharing arrangement/POP (% of revenue)
+    nat_gas_processor_share = db.Column(db.Float)
+    ngl_processor_share = db.Column(db.Float)
+    helium_processor_share = db.Column(db.Float)
+
+    # production taxes
+    severance_tax = db.Column(db.Float)
+    ad_valorem_tax = db.Column(db.Float)
+
+    # capex assumptions
+    capex_assumptions = db.Column(db.String, nullable=False)
+
+    # gas concentration assumptions
+    gas_concentrations = db.Column(db.String)
+
+    # refinary assumptions
+    # percentage eefficiency
+    refinery_assumptions = db.Column(db.String)
+
+    # drilling_start_date
+    drilling_start_date = db.Column(db.String)
+
+    # production_start_date
+    prod_start_date = db.Column(db.String)
+
+
+    # RELATIONSHIPS
+    well = db.relationship('Well', back_populates='assumptions')
+
+
+    # serialize rules
+    serialize_rules = ('-wells_table',)
+
+    # validations
+    # for any future modification
+
+
+class ProductionCurve(db.Model, SerializerMixin):
+    __tablename__ = 'production_curve_table'
+
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    type_curve = db.Column(db.String, nullable=False)
+
+    # RELATIONSHIPS
+    well = db.relationship('Well', back_populates='production_curve')
+
+    # serialize rules
+    serialize_rules = ('-wells_table',)
+
+    # valiations
+    # (need to think about this)
+
+class Project(db.Model, SerializerMixin):
+    __tablename__ = 'projects_table'
+
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # RELATIONSHIPS
+    wells = db.relationship('Well', back_populates='project')
+
+    # serialize rules
+    serialize_rules = ('-wells_table',)
+
+
+class Pricing(db.Model, SerializerMixin):
+    __tablename__ = "Pricing_table"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    oil_price = db.Column(db.Float)
+    methane_price = db.Column(db.Float)
+    helium_price = db.Column(db.Float)
+    ethane_price =  db.Column(db.Float)
+    propane_price =  db.Column(db.Float)
+    i_butane_price  = db.Column(db.Float)
+    n_butane_price = db.Column(db.Float)
+    i_pentane_price = db.Column(db.Float)
+    n_pentane_price = db.Column(db.Float)
+    hexane_plus_price = db.Column(db.Float)
+
 
 
 
