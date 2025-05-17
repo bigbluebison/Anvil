4e8a70f3c05f251be4ab854e085101572b371cf1
Baran <bar.yildiran@gmail.com>
2025-05-17 14:17:43 -0600

    new

--- server/singlewellfinmodel.py
+++ server/singlewellfinmodel.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import json
 from dateutil.relativedelta import relativedelta
-
+import datetime
 
 from app import app
-from models import Well
+from models import Well, Assumptions, ProductionCurve, Project, Pricing
 
 
 def get_typecurve(id):
@@ -20,20 +20,46 @@ def get_typecurve(id):
     type_curve_data = json.loads(type_curve)
     type_curve_df = pd.DataFrame(type_curve_data)
 
-    production_start_date = well_to_dict['assumptions']['prod_start_date']
+    # covert the 'Date' column to datetime
+    production_start_date = datetime.datetime.strptime(
+        well_to_dict['assumptions']['prod_start_date'], "%Y-%m-%d"
+    )
 
     number_of_rows = len(type_curve_df)
 
     # Create a list of dates incremented by one month for each entry
-    dates = [(production_start_date + relativedelta(months=+i)).strftime("%m/%d/%Y") for i in range(number_of_rows)]
-    type_curve_df.insert(0, "Date", dates)
+    dates = pd.date_range(start=production_start_date, periods=number_of_rows, freq='MS')
+    type_curve_df.insert(0, "Date", dates.strftime("%m/%d/%Y"))
 
+    # month number of the date
+    # year number of the date
+
+    type_curve_df.insert(1, "Month_Number", dates.month)
+    type_curve_df.insert(2, "Year_Number", dates.year)
+
+    # fixing the formatting of production figures
+    type_curve_df.iloc[:,-2] = type_curve_df.iloc[:,-2].str.replace(',', '').astype(float)
+    type_curve_df.iloc[:,-1] = type_curve_df.iloc[:,-1].str.replace(',', '').astype(float)
 
     return type_curve_df
 
 
+def working_interest(id):
+    """
+    Get the working interest for a given well ID.
+    """
+
+    assumptions_to_get = Assumptions.query.filter_by(id=id).first()
+    assumptions_to_dict = assumptions_to_get.to_dict()
+    working_interest_before = assumptions_to_dict['wi_before_payout']
+    working_interest_after = assumptions_to_dict['wi_after_payout']
+    net_working_interest_before = assumptions_to_dict['nri_before_payout']
+    net_working_interest_after = assumptions_to_dict['nri_after_payout']
+
+
+    return working_interest_before, working_interest_after, net_working_interest_before, net_working_interest_after
 
 
 with app.app_context():   
-        print(get_typecurve(1))
+        print(working_interest(1))
 
